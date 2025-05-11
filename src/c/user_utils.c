fa1b58702829d2b6d137d7f731ce08723e9b0f19
Your Name <afifhabiburrahman21@gmail.com>
2025-05-11 11:22:18 +0700

    feat: implementasi fungsi utility pada user.h

---
+++ src/c/user_utils.c
@@ -0,0 +1,23 @@
+#include "../header/user.h"
+
+int user_count = 0; // Implementasi variabel global
+User* current_user = NULL; // Implementasi variabel global
+
+const char* role_to_string(UserRole role) {
+    switch(role) {
+        case ROLE_MANAGER: return "Manager";
+        case ROLE_DOKTER: return "Dokter";
+        case ROLE_PASIEN: return "Pasien";
+        default: return "Unknown";
+    }
+}
+
+boolean is_alpha_string(const char* str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!((str[i] >= 'A' && str[i] <= 'Z') || 
+              (str[i] >= 'a' && str[i] <= 'z'))) {
+            return false;
+        }
+    }
+    return true;
+}
