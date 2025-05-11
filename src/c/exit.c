2b9b1b6900d2c4d82278fbc27ebc0a2642be55df
Your Name <afifhabiburrahman21@gmail.com>
2025-05-11 11:22:42 +0700

    feat: menambah implementasi exit

---
+++ src/c/exit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../header/exit.h"
+#include "../header/user.h" // Untuk akses current_user
+
+void exit_system(ListUser *users) {
+    // Cek apakah ada pengguna yang sedang login
+    if (current_user == NULL) {
+        printf("\nTidak ada pengguna yang sedang login. Silakan login terlebih dahulu untuk keluar.\n");
+        return;
+    }
+
+    char input[10];
+
+    printf("\nApakah Anda mau keluar dari rumah sakit? (y/n)\n");
+
+    while (1) {
+        printf("Pilihan Anda: ");
+        scanf("%s", input);
+
+        if (strcmp(input, "Y") == 0 || strcmp(input, "y") == 0) {
+            printf("\n%s keluar dari Rumah Sakit,\nSampai jumpa!\n", current_user->username);
+            exit(0);
+        } else if (strcmp(input, "N") == 0 || strcmp(input, "n") == 0) {
+            printf("\nKembali ke menu utama...\n");
+            return;
+        } else {
+            printf("Tidak valid. Pilih Y atau N ya!\n");
+        }
+    }
+}
---
+++ src/header/exit.h
@@ -0,0 +1,12 @@
+
+#ifndef __EXIT_H__
+#define __EXIT_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "boolean.h"
+#include "../ADT/header/list_user.h"
+
+void exit_system(ListUser *users);
+
+#endif
