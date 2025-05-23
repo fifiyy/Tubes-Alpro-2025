a19968d7fac401022835068f46aac0ad315e9948
Your Name <afifhabiburrahman21@gmail.com>
2025-05-23 21:40:39 +0700

    feat: menambah ADT queue.c

---
+++ src/ADT/c/queue.c
@@ -0,0 +1,50 @@
+#include "../header/queue.h"
+
+void queue_init_queue(struct Queue *q) {
+    q->count = 0;
+}
+
+bool queue_is_empty(struct Queue *q) {
+    return q->count == 0;
+}
+
+bool queue_is_full(struct Queue *q) {
+    return q->count == MAX_SIZE;
+}
+
+void queue_enqueue(struct Queue *q, int value) {
+    if (!isFull(q)) {
+        q->items[q->count] = value;
+        q->count++;
+    }
+    else {
+        printf("Queue penuh!\n");
+    }
+}
+
+int queue_dequeue(struct Queue *q) {
+    if (!isEmpty(q)) {
+        int value = q->items[0];
+        for (int i = 1; i < q->count; i++) {
+            q->items[i - 1] = q->items[i];
+        }
+        q->count--;
+        return value;
+    } else {
+        printf("Queue kosong!\n");
+        return SENTINEL;
+    }
+}
+
+int queue_front(struct Queue *q) {
+    if (!isEmpty(q)) {
+        return q->items[0];
+    } else {
+        printf("Queue kosong!\n");
+        return SENTINEL;
+    }
+}
+
+int queue_size(struct Queue *q) {
+    return q->count;
+}
