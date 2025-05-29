4e58f830b8bcdcf8bfaa949bbac36f8b95a1aa3a
aucloire <ellainejh@gmail.com>
2025-05-29 15:45:51 +0700

    feat: menambah dokter.h

---
+++ src/header/dokter.h
@@ -0,0 +1,22 @@
+#ifndef DOKTER_H
+#define DOKTER_H
+
+#include "antrian.h"
+
+#define MAX_PASIEN_RUANGAN 3
+
+typedef struct {
+    int id;
+    char nama[16];
+    char ruangan;
+    Pasien *pasienDiRuangan[MAX_PASIEN_RUANGAN];
+    AntrianDokter antrian;
+} Dokter;
+
+/*
+    Kalau jumlahPasienDiRuangan < 3, langsung masuk ke ruangan
+    Tapi kalau jumlahPasienDiRuangan = 3, masuk ke antrian
+*/
+Pasien *assignPasienKeDokter (Dokter *dokter, Pasien *pasien);
+
+#endif
