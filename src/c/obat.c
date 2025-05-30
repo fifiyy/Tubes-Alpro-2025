1ff6f43528c463ef299a7e942a8402339a86e6fa
aucloire <ellainejh@gmail.com>
2025-05-30 14:26:25 +0700

    feat: perbaiki obat.c

--- src/c/obat.c
+++ src/c/obat.c
@@ -1,20 +1,16 @@
-#include "obat.h"
+#include "../header/obat.h"
+
+Obat ketObat[] = {
+    {1, "Oseltamivir"},
+    {2, "Vitamin C"},
+    {3, "Remdesivir"},
+    {4, "Lisinopril"},
+    {5, "Metfomin"},
+};
 
 void initObat (DatabaseObat *dbO) {
-    Obat obat1 = {.id = 1, .nama = "Oseltamivir"};
-    dbO->daftar[0] = obat1;
-
-    Obat obat2 = {.id = 2, .nama = "Vitamin C"};
-    dbO->daftar[1] = obat2;
-
-    Obat obat3 = {.id = 3, .nama = "Remdesivir"};
-    dbO->daftar[2] = obat3;
-
-    Obat obat4 = {.id = 4, .nama = "Lisinopril"};
-    dbO->daftar[3] = obat4;
-
-    Obat obat5 = {.id = 5, .nama = "Metfomin"};
-    dbO->daftar[4] = obat5;
-
+    for (int i = 0; i < 5; i++) {
+        dbO->daftar[i] = ketObat[i];
+    }
     dbO->jumlahObat = 5;
 }
