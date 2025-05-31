ee3552677c1e1c80203690c2ff50636056bb52bf
Your Name <afifhabiburrahman21@gmail.com>
2025-06-01 06:54:28 +0700

    feat: membuat start_rs untuk menampung fungsi pada main sebelumnya

---
+++ src/c/Start_Rs.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include "../header/Display.h"
+#include "../header/User.h"
+#include "../header/Login.h"
+#include "../header/Register.h"
+#include "../header/Logout.h"
+#include "../header/Lupa_Password.h"
+#include "../header/Help.h"
+#include "../header/Init_Data.h"
+#include "../header/Exit.h"
+#include "../header/F10.h"
+#include "../header/Lihat_Denah.h"
+#include "../header/Daftar_Check_Up.h"
+#include "../header/Diagnosis.h"
+#include "../header/Ngobatin.h"
+#include "../header/Antrian_Saya.h"
+#include "../header/Minum_Obat.h"
+#include "../header/Minum_Penawar.h"
+#include "Pulangdok.h"
+
+ListUser users;
+ListRuangan ruangan;
+Set usernames;
+Set doctor_usernames;
+extern User *currUser;
+
+void run_rs() {
+    init_data(&users, &usernames);
+    init_ruang(&ruangan, &users);
+    currUser = NULL;
+
+    char command[20];
+    int nomor_ruangan;
+
+    while (1) {
+        // Tampilkan menu sesuai role
+        if (currUser == NULL) {
+            display_main_menu();
+            printf("\n>>> ");
+        } else if (currUser->role == ROLE_PASIEN) {
+            display_status_user(currUser->username, &(currUser->role));
+            // display_menu_pasien();
+            printf("\n>>> ");
+        } else if (currUser->role == ROLE_DOKTER) {
+            display_status_user(currUser->username, &(currUser->role));
+            // display_menu_dokter();
+            printf("\n>>> ");
+        } else if (currUser->role == ROLE_MANAGER) {
+            display_status_user(currUser->username, &(currUser->role));
+            // display_menu_manajer();
+            printf("\n>>> ");
+        }
+
+        // printf("\n>>> ");
+        if (scanf("%19s", command) != 1) {
+            printf("Input tidak valid!\n");
+            while (getchar() != '\n');
+            continue;
+        }
+        
+        if (strcmp(command, "LOGIN") == 0) {
+            login_system(&users);
+        } else if (strcmp(command, "REGISTER") == 0) {
+            register_pasien(&users, &usernames);
+        } else if (strcmp(command, "LOGOUT") == 0) {
+            logout_system();
+        } else if (strcmp(command, "LUPA_PASSWORD") == 0) {
+            lupa_password_system(&users);
+        } else if (strcmp(command, "HELP") == 0) {
+            help_system();
+        } else if (strcmp(command, "TAMBAH_DOKTER") == 0) {
+                tambah_dokter(&users, &usernames);
+        } else if (strcmp(command, "ASSIGN_DOKTER") == 0) {
+                assign_dokter(&users, &ruangan);
+        } else if (strcmp(command, "EXIT") == 0) {
+            exit_system(&users);
+        } else if (strcmp(command, "LIHAT_DENAH") == 0) {
+            lihat_denah(ruangan);
+        } else if (strcmp(command, "LIHAT_SEMUA_ANTRIAN") == 0) {
+                lihat_semua_antrian(&ruangan, ruangan.jumlah, &users);
+        } else if (strcmp(command, "LIHAT_RUANGAN") == 0) {
+            printf("Masukkan nomor ruangan (1-%d): ", ruangan.jumlah);
+            if (scanf("%d", &nomor_ruangan) == 1) {
+                if (nomor_ruangan >= 1 && nomor_ruangan <= ruangan.jumlah) {
+                    lihat_ruangan(&ruangan, nomor_ruangan, &users);
+                } else {
+                    printf("Nomor ruangan tidak valid! (1-%d)\n", ruangan.jumlah);
+                }
+            } else {
+                printf("Input tidak valid!\n");
+            }
+        } else if (strcmp(command, "DAFTAR_CHECKUP") == 0) {
+            daftar_check_up(currUser, users.data, users.length, &ruangan);
+        } else if (strcmp(command, "LIHAT_RUANGAN_SAYA") == 0) {
+            lihat_ruangan_saya(&ruangan, &users);
+        } else if (strcmp(command, "LIHAT_DAFTAR_PASIEN") == 0) {
+            lihat_daftar_pasien(&ruangan, &users);
+        } else if (strcmp(command, "DIAGNOSIS") == 0) {
+            diagnosis_pasien(currUser);
+        } else if (strcmp(command, "NGOBATIN") == 0) {
+            ngobatin(currUser, users.data, users.length, &ruangan);
+        } else if (strcmp(command,"LIHAT_ANTRIAN") == 0) {
+                cek_antrian_saya(currUser, users.data, &ruangan, users.length);
+        } else if (strcmp(command, "MINUM_OBAT") == 0) {
+                minum_obat(currUser);
+        } else if (strcmp(command, "MINUM_PENAWAR") == 0) {
+            minum_penawar(currUser);
+        } else if (strcmp(command, "PULANG") == 0) {
+            pulangdok(currUser, &ruangan);
+        }  else {
+            printf("Command tidak dikenali. Ketik HELP untuk bantuan.\n");
+        }
+    }
+    return; 
+}
---
+++ src/header/Start_Rs.h
@@ -0,0 +1,6 @@
+#ifndef START_RS_H
+#define START_RS_H
+
+void run_rs();
+
+#endif
