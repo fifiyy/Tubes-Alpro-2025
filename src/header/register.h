aba2cd67ce395a3caa093e9e182f5ef66055ccdd
Your Name <afifhabiburrahman21@gmail.com>
2025-06-01 06:46:51 +0700

    update: menambah include stack untuk inisialisasi perutPasien

--- src/header/register.h
+++ src/header/register.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include "../ADT/header/List_User.h"
 #include "../ADT/header/Set.h"
+#include "../ADT/header/Stack.h"
 
 boolean is_username_unique(const Set *usernames, const char* username);
 void register_pasien(ListUser *users, Set *usernames);
